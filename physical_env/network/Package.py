5b914d3b1f0a93d9f64c79ccb6669c152085cb30
manggobrrbrr <tanh140703@gmail.com>
2024-04-25 19:53:00 +0700

    change somthing bug

--- physical_env/network/Package.py
+++
@@ -1,4 +0,0 @@
-class Package:
-    def __init__(self, target_id, size):
-        self.package_size = size
-        self.target_id = target_id
--- physical_env/network/Target.py
+++
@@ -1,6 +0,0 @@
-
-
-class Target:
-    def __init__(self, location):
-        self.location = location
-        self.id = None
